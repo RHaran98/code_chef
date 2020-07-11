4f9a8881ec9da950277b68e4b5861afee85de2af
KiranbaskarV <kiran8402@gmail.com>
2020-07-11 15:09:43 +0530

    Added dynamic arrays and fixed combine procedure

--- TSORT_kb.cpp
+++ TSORT_kb.cpp
@@ -4,16 +4,17 @@ int number_list[1000000];
 
 void mergesort(int start_ind, int end_ind)
 {
-	int mid = (start_ind + end_ind) / 2;
-	if(start_ind != end_ind)
-	{
+	if(start_ind < end_ind) {
+		int mid = (start_ind + end_ind) / 2;
+		
 		mergesort(start_ind, mid);
 		mergesort(mid + 1, end_ind);
-		
-		constexpr int s1 = mid - start_ind + 1;
-		constexpr int s2 = end_ind - mid;
 
-		int arr1[s1], arr2[s2];
+		int s1 = mid - start_ind + 1;
+		int s2 = end_ind - mid;
+
+		int *arr1 = new int [s1];
+		int *arr2 = new int [s2];
 
 		for(int i = 0; i < s1; i++)
 		{
@@ -26,19 +27,22 @@ void mergesort(int start_ind, int end_ind)
 		}
 
 		int index = start_ind;
-		for(int i = 0, j = 0; i <= s1 || j <= s2;)
+		for(int i = 0, j = 0; i < s1 || j < s2;)
 		{
-			if(j == s2 + 1 || arr1[i] <= arr2[j])
+			if(j == s2 || arr1[i] <= arr2[j])
 			{
-				number_list[index] = number_list[i];
+				number_list[index] = arr1[i];
 				i++; index++;
 			}
-			else if(i == s1 + 1 || arr2[j] < arr1[i])
+			else if(i == s1 || arr2[j] < arr1[i])
 			{
-				number_list[index] = number_list[j];
+				number_list[index] = arr2[j];
 				j++; index++;
 			}
 		}
+
+		delete [] arr1;
+		delete [] arr2;
 	}
 }
 
