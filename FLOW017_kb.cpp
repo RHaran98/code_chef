a89590ba98988d0c68ff31e3f5ea502cc999f2d0
KiranbaskarV <kiran8402@gmail.com>
2020-07-12 21:30:39 +0530

    Second Largest soln

---
+++ FLOW017_kb.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	int tc;
+	std::cin>>tc;
+	while(tc--)
+	{
+		int a,b,c;
+		std::cin>>a>>b>>c;
+		if((a > b && a < c) || (a < b && a > c)) {
+			std::cout<<a<<"\n";
+		}
+		else if((b > a && b < c) || (b < a && b > c)) {
+			std::cout<<b<<"\n";
+		}
+		else {
+			std::cout<<c<<"\n";
+		}
+	}
+}
