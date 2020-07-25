e5d070ef693e94ba4df3a8f1e1c69430a86efb84
KiranbaskarV <kiran8402@gmail.com>
2020-07-25 23:12:02 +0530

    Chef and two strings soln

---
+++ CHEFSTLT_kb.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+	int tc;
+	std::cin>>tc;
+	while(tc--)
+	{
+		string s1, s2;
+		std::cin>>s1>>s2;
+
+		int qcount = 0;
+		int diff = 0;
+
+		for(int i = 0; i < s1.length(); i++)
+		{
+			if (s1[i] == '?'|| s2[i] == '?') qcount++;
+			else
+			{
+				if(s1[i] != s2[i]) diff++;
+			}
+		}
+
+		int min = diff;
+		int max = diff + qcount;
+		std::cout<<min<<" "<<max<<"\n";
+	}
+}
