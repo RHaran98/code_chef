6e67a273872fdb30ec2676e8957d5a95fb234850
KiranbaskarV <kiran8402@gmail.com>
2020-07-28 21:15:56 +0530

    Rectangle soln

---
+++ RECTANGL_kb.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	int tc;
+	std::cin>>tc;
+	while(tc--)
+	{
+		int a,b,c,d;
+		std::cin>>a>>b>>c>>d;
+		
+		bool isRectangle = false;
+		if(a == b && c == d) isRectangle = true;
+		else if(a == c && b == d) isRectangle = true;
+		else  if(a == d && b == c) isRectangle = true;
+
+		if(isRectangle) std::cout<<"YES\n";
+		else std::cout<<"NO\n";
+}
